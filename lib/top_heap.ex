3f8fa8ba0cf7beb1f31f954029ee84e87b8b51d5
Yuriy Zhar <darkyfelis@gmail.com>
2025-04-30 19:49:32 +0200

    fixed return of gb_trees

--- lib/top_heap.ex
+++ lib/top_heap.ex
@@ -1,7 +1,14 @@
 defmodule Fuzler.TopHeap do
+  @moduledoc false
   # min-heap limited to N elements
+
+  @doc false
+  @spec new(any()) :: {:gb_trees.tree(any(), any()), any()}
   def new(n), do: {:gb_trees.empty(), n}
 
+  @doc false
+  @spec push_top(any(), {:gb_trees.tree(any(), any()), any()}) ::
+          {:gb_trees.tree(any(), any()), any()}
   def push_top(item = {_k, _v, score}, {heap, n}) do
     heap =
       if :gb_trees.size(heap) < n do
@@ -9,7 +16,7 @@ defmodule Fuzler.TopHeap do
       else
         case :gb_trees.smallest(heap) do
           {min_score, _} when score > min_score ->
-            {_old, heap} = :gb_trees.take_smallest(heap)
+            {_, _, heap} = :gb_trees.take_smallest(heap)
             :gb_trees.enter(score, item, heap)
 
           _ ->
@@ -20,6 +27,8 @@ defmodule Fuzler.TopHeap do
     {heap, n}
   end
 
+  @doc false
+  @spec to_desc_list({:gb_trees.tree(any(), any()), any()}) :: [any()]
   def to_desc_list({heap, _n}) do
     heap |> :gb_trees.to_list() |> Enum.reverse() |> Enum.map(&elem(&1, 1))
   end
