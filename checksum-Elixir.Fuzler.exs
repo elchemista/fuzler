241912a20dd4e0b88c40afffb42aa6d42cbe7045
Yuriy Zhar <darkyfelis@gmail.com>
2025-05-01 08:30:36 +0200

    added checksum

---
+++ checksum-Elixir.Fuzler.exs
@@ -0,0 +1,12 @@
+%{
+  "fuzler-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:00148dfe8c5abd4240d016e770416ac32e05e9b39c84955e5723167219e570c9",
+  "fuzler-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:191cf42b17c83d53d5e6620cc2700f4ccb39cba53da3f712ce6365b1772e565c",
+  "libfuzler-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:8076769fb460cc8d4fe06d6cb16a503f6b9470fa1d9a563fd5caab2d6ff71919",
+  "libfuzler-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:92ab3869f6a08f38c0119f5cb6c62fdb1326faf7f42cbf13e9e6c2b0d9046308",
+  "libfuzler-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:201e8f36a71bac0e57166020833b7a140a6996c9a002be63afedaa4b6ed859a3",
+  "libfuzler-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:f7af84649a7918bda4a33bf5ac8f52f2495d9f99aea29cd15dc62d35206e0abd",
+  "libfuzler-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:be50c300f0ac6e8f8574e8feabc2752736761cf099bc448dc84e802d65959ac9",
+  "libfuzler-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:425163158a0746dd19a7a42639bbcc686e14955fb21de50f21199fb7d788fcc8",
+  "libfuzler-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:bc12f2a360aeafbdb8bd662fbdd8f4fdf2b7e1b678823e66fd3930bfea8cf20c",
+  "libfuzler-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:b8ed328b17489d398461e755b1425592cefee895793e50fbec222cdad40e4906",
+}
