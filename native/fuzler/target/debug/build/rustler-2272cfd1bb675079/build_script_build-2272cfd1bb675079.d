a0360ca66903c96f510101252a5425cc843459c8
Yuriy Zhar <darkyfelis@gmail.com>
2025-05-01 08:15:37 +0200

    removed target debug

--- native/fuzler/target/debug/build/rustler-2272cfd1bb675079/build_script_build-2272cfd1bb675079.d
+++
@@ -1,5 +0,0 @@
-/home/dev/Sviluppo/personal/fuzler/native/fuzler/target/debug/build/rustler-2272cfd1bb675079/build_script_build-2272cfd1bb675079: /home/dev/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustler-0.36.1/build.rs
-
-/home/dev/Sviluppo/personal/fuzler/native/fuzler/target/debug/build/rustler-2272cfd1bb675079/build_script_build-2272cfd1bb675079.d: /home/dev/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustler-0.36.1/build.rs
-
-/home/dev/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustler-0.36.1/build.rs:
--- native/fuzler/target/debug/deps/fuzler.d
+++
@@ -1,5 +0,0 @@
-/home/dev/Sviluppo/personal/fuzler/native/fuzler/target/debug/deps/libfuzler.so: src/lib.rs
-
-/home/dev/Sviluppo/personal/fuzler/native/fuzler/target/debug/deps/fuzler.d: src/lib.rs
-
-src/lib.rs:
